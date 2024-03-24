721a432328d731b0ed9465c75350744422075c84
Shihabulislam1 <mdhshihabulislam.nte.ruet@gmail.com>
2024-03-24 19:00:37 +0600

    font import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from './ui/font';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
